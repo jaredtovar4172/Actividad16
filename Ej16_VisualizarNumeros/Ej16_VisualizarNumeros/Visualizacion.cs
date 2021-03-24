f28add581b7f1432f9cd94790e68bf0c1a7b60dd
jaredtovar4172 <80796166+jaredtovar4172@users.noreply.github.com>
2021-03-23 21:13:58 -0600

    Add files via upload

---
+++ Ej16_VisualizarNumeros/Ej16_VisualizarNumeros/Visualizacion.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ej16_VisualizarNumeros
+{
+    class Visualizacion
+    {
+        public void Figuras (ListBox lista)
+        {
+            string n = "";
+            int m = 3;
+            for (int i = 1; i <= 7; i++)
+            {
+                if (i <= 4)
+                {
+                    for (int j = 1; j <= i; j++)
+                    {
+                        n = n + j;
+                    }
+                }
+                if (i>=5)
+                {
+                    for (int x = 1; x <= m; x++)
+                    {
+                       n = n + x;
+                    }
+                  m--;
+                }
+                lista.Items.Add(n);
+                n = "";
+            }
+        }
+    }
+}
